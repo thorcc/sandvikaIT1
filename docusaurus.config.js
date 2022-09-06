b9dc85005d13f7ccf7698e9c63f877c2b6309855
thorcc <thorchrcow@gmail.com>
2022-09-06 09:13:10 +0200

    fiksa config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,27 +79,22 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        magicComments: [
+          // Remember to extend the default highlight class name as well!
+          {
+            className: 'theme-code-block-highlighted-line',
+            line: 'highlight-next-line',
+            block: {start: 'highlight-start', end: 'highlight-end'},
+          },
+          {
+            className: 'code-block-error-line',
+            line: 'This will error',
+          },
+        ],
       },
     }),
 };
 
-module.exports = {
-  themeConfig: {
-    prism: {
-      magicComments: [
-        // Remember to extend the default highlight class name as well!
-        {
-          className: 'theme-code-block-highlighted-line',
-          line: 'highlight-next-line',
-          block: {start: 'highlight-start', end: 'highlight-end'},
-        },
-        {
-          className: 'code-block-error-line',
-          line: 'This will error',
-        },
-      ],
-    },
-  },
-};
+
 
 module.exports = config;
