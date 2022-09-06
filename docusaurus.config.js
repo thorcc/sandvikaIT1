3b04f74d990504fae1ed8b319f0be36dfd73955e
thorcc <thorchrcow@gmail.com>
2022-09-06 08:52:15 +0200

    fjern edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,8 +37,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/sandvika-VGS/sandvika-VGS.github.io',
+          // editUrl:
+          //   'https://github.com/thorcc/sandvikaIT1',
         },
         blog: false,
         theme: {
